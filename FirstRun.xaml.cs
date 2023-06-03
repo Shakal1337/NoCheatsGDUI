4f5408cafa720b34662cf122de6768b9875629ed
Shakal1337 <InS.Network.Help@outlook.com>
2023-06-03 23:52:30 +0300

    dev mode comleted

---
+++ FirstRun.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.VisualBasic;
+using Microsoft.Win32;
+
+
+namespace NoCheatsGD_UI_
+{
+    /// <summary>
+    /// Логика взаимодействия для FirstRun.xaml
+    /// </summary>
+    public partial class FirstRun : Window
+    {
+        OpenFileDialog _dlg = new OpenFileDialog();
+        public FirstRun()
+        {
+            InitializeComponent();
+            this.Topmost = true;
+            if (Settings.Default.DebugMode == true) 
+            {
+                this.DebugButton.Visibility = Visibility.Visible;
+            }
+            else 
+            {
+                this.DebugButton.Visibility = Visibility.Hidden;
+            }
+
+        }
+
+        private void InfoShow(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show
+                (
+                "By shakal" +
+                "\nconntact:" +
+                "\nemail: InS.Network.Help@outlook.com" +
+                "\ntelegram: @shakalofff" +
+                "\nInS 2020-2023 ©",
+                "Info",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information
+                );
+        }
+
+        private void bypasspath(object sender, RoutedEventArgs e)
+        {
+            Nullable<bool> ress = _dlg.ShowDialog();
+            string pathbypass = _dlg.FileName;
+            if ( ress != null ) {
+                _ = pathbypass;
+                Settings.Default.GDHMMoveSettingsByPass = pathbypass;
+                Settings.Default.Save();
+            
+            }
+        }
+
+        private void playerpath(object sender, RoutedEventArgs e)
+        {
+            Nullable<bool> ress = _dlg.ShowDialog();
+            string pathplayer = _dlg.FileName;
+            if ( ress != null ) {
+                _ = pathplayer;
+                Settings.Default.GDHMMoveSettingsPlayer = pathplayer;
+                Settings.Default.Save();
+            }
+        }
+
+        private void globalpath(object sender, RoutedEventArgs e)
+        {
+            Nullable<bool> ress = _dlg.ShowDialog();
+            string pathglobal = _dlg.FileName;
+            if ( ress != null )
+            {
+                _ = pathglobal;
+                Settings.Default.GDHMMoveSettingsGlobal = pathglobal;
+                Settings.Default.Save();
+            }
+        }
+
+        private void gdbotpath(object sender, RoutedEventArgs e)
+        {
+            Nullable<bool> ress = _dlg.ShowDialog();
+            string pathgdbot = _dlg.FileName;
+            if ( ress != null )
+            {
+                _ = pathgdbot;
+                Settings.Default.GDBotMoveSettings = pathgdbot;
+            }
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.FirstRun = false;
+            Settings.Default.Save();
+            FirstRun firstRun = new FirstRun();
+            firstRun.Hide();
+        }
+
+        private void Reset(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.GDBotMoveSettings = "";
+            Settings.Default.GDHMMoveSettingsByPass = "";
+            Settings.Default.GDHMMoveSettingsCore = "";
+            Settings.Default.GDHMMoveSettingsCreator = "";
+            Settings.Default.GDHMMoveSettingsGlobal = "";
+            Settings.Default.GDHMMoveSettingsPlayer = "";
+            Settings.Default.GDHMMoveSettingsvVriables = "";
+            Settings.Default.Save();
+        }
+
+        private void corepath(object sender, RoutedEventArgs e)
+        {
+            Nullable<bool> ress = _dlg.ShowDialog();
+            string pathcore = _dlg.FileName;
+            if ( ress != null )
+            {
+                _ = pathcore;
+               Settings.Default.GDHMMoveSettingsCore = pathcore;
+               Settings.Default.Save();
+            }
+        }
+
+        private void creatorpath(object sender, RoutedEventArgs e)
+        {
+            Nullable<bool> ress = _dlg.ShowDialog();
+            string pathcreator = _dlg.FileName;
+            if (ress != null) {
+                _ = pathcreator;
+                Settings.Default.GDHMMoveSettingsCreator = pathcreator;
+                Settings.Default.Save();
+            }
+        }
+
+        private void variablespath(object sender, RoutedEventArgs e)
+        {
+            Nullable<bool> ress = _dlg.ShowDialog();
+            string variablespath = _dlg.FileName;
+            if ( ress != null )
+            {
+                _ = variablespath;
+                Settings.Default.GDHMMoveSettingsvVriables = variablespath;
+                Settings.Default.Save();
+            }
+        }
+
+        private void DebugActived(object sender, MouseWheelEventArgs e)
+        {
+            this.DebugButton.Visibility = Visibility.Visible;
+            MessageBox.Show
+                (
+                "Debug Active only this window",
+                "",
+                MessageBoxButton.OK,
+                MessageBoxImage.Warning
+                );
+        }
+
+        private void DubugWindow(object sender, RoutedEventArgs e)
+        {
+            debug db = new debug();
+            db.Show();
+        }
+    }
+}
---
+++ debug.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace NoCheatsGD_UI_
+{
+    /// <summary>
+    /// Логика взаимодействия для debug.xaml
+    /// </summary>
+    public partial class debug : Window
+    {
+        public debug()
+        {
+            InitializeComponent();
+            if (Settings.Default.DebugMode == true)
+            {
+                this.ActiveDubug.IsChecked = true;
+            }
+            else 
+            {   
+                this.ActiveDubug.IsChecked = false;
+            }
+        }
+
+        private void ActiveDebugModeFull(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.DebugMode = true;
+            Settings.Default.Save();
+            this.wairnongdebug.Visibility = Visibility.Hidden;
+            this.activedubugmessage.Visibility = Visibility.Visible;
+        }
+
+        private void DebugModeDiactived(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.DebugMode = false;
+            Settings.Default.Save();
+            this.wairnongdebug.Visibility = Visibility.Visible;
+            this.activedubugmessage.Visibility = Visibility.Hidden;
+        }
+
+        private void AllSettingsView_(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show
+                (
+                $"Main floder: {Settings.Default.MainFloder}" +
+                $"\nPath to mods: {Settings.Default.MainFloderResalutFloder}" +
+                $"\npath to gdbot: {Settings.Default.GDBotMoveSettings}" +
+                $"\npath to gdhm bypass: {Settings.Default.GDHMMoveSettingsByPass}" +
+                $"\npath to gdhm player: {Settings.Default.GDHMMoveSettingsPlayer}" +
+                $"\npath to gdhm core: {Settings.Default.GDHMMoveSettingsCore}" +
+                $"\npath to gdhm creator: {Settings.Default.GDHMMoveSettingsCreator}" +
+                $"\npath to gdhm vriables: {Settings.Default.GDHMMoveSettingsvVriables}",
+                "",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information
+                );
+        }
+
+        private void AllSettingsView(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show
+                (
+                $"Main floder: {Settings.Default.MainFloder}" +
+                $"\nPath to mods: {Settings.Default.MainFloderResalutFloder}" +
+                $"\npath to gdbot: {Settings.Default.GDBotMoveSettings}" +
+                $"\npath to gdhm bypass: {Settings.Default.GDHMMoveSettingsByPass}" +
+                $"\npath to gdhm player: {Settings.Default.GDHMMoveSettingsPlayer}" +
+                $"\npath to gdhm core: {Settings.Default.GDHMMoveSettingsCore}" +
+                $"\npath to gdhm creator: {Settings.Default.GDHMMoveSettingsCreator}" +
+                $"\npath to gdhm vriables: {Settings.Default.GDHMMoveSettingsvVriables}" +
+                $"\ndebug mod status: {Settings.Default.DebugMode}" +
+                $"\nfirst run: {Settings.Default.FirstRun}",
+                "",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information
+                );
+        }
+
+        private void OpenMainFloder(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer.exe", @"C:\Users\1337\Desktop\dev");
+        }
+
+        private void OpenModsFloder(object sender, RoutedEventArgs e)
+        {
+            Process.Start("explorer.exe", @"C:\Users\1337\Desktop\dev");
+        }
+
+        private void ResetAll(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.Reset();
+            Settings.Default.Save();
+        }
+
+        private void ResetPath(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.GDBotMoveSettings = "";
+            Settings.Default.GDHMMoveSettingsByPass = "";
+            Settings.Default.GDHMMoveSettingsCore = "";
+            Settings.Default.GDHMMoveSettingsCreator = "";
+            Settings.Default.GDHMMoveSettingsGlobal = "";
+            Settings.Default.GDHMMoveSettingsPlayer = "";
+            Settings.Default.GDHMMoveSettingsvVriables = "";
+            Settings.Default.Save();
+        }
+    }
+}
