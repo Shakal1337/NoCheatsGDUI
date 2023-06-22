8e18ed9fd08a3536db16e7a4055a010dc0eb13a6
Shakal1337 <InS.Network.Help@outlook.com>
2023-06-22 04:47:31 +0300

    add storybord action

--- obj/Debug/net7.0-windows7.0/FirstRun.g.i.cs
+++ obj/Debug/net7.0-windows7.0/FirstRun.g.i.cs
@@ -77,7 +77,7 @@ namespace NoCheatsGD_UI_ {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/NoCheatsGD(UI);V1.0.0.0;component/firstrun.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/NoCheatsGD(UI);component/firstrun.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\FirstRun.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
